ffd335a4834b41230fb4f16c6843aa370b1c9afa
Tran Thi Nha <tranthinha160296@gmail.com>
2016-10-23 21:14:57 +0700

    clear map before search

--- app/src/main/java/com/example/thinha/newyorktimesarticlesearch/activities/MainActivity.java
+++ app/src/main/java/com/example/thinha/newyorktimesarticlesearch/activities/MainActivity.java
@@ -53,46 +53,45 @@
 
         public class MainActivity extends AppCompatActivity {
 
-    private StaggeredGridLayoutManager mLayoutManager;
-    @BindView(R.id.rvResult)
-    RecyclerView rvResult;
+            private StaggeredGridLayoutManager mLayoutManager;
+            @BindView(R.id.rvResult)
+            RecyclerView rvResult;
 
-    @BindView(R.id.pbLoading)
-    ProgressBar pbLoading;
+            @BindView(R.id.pbLoading)
+            ProgressBar pbLoading;
 
-    @BindView(R.id.pbLoadMore)
-    ProgressBar pbLoadingMore;
+            @BindView(R.id.pbLoadMore)
+            ProgressBar pbLoadingMore;
 
-    ArticleAdapter adapter;
+            ArticleAdapter adapter;
 
-    int pageNumber = 1;
-    MenuItem searchItem ,sortItem;
-    Map map;
+            int pageNumber = 1;
+            MenuItem searchItem ,sortItem;
+            Map map;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                ButterKnife.bind(this);
 
-        if (!isNetworkAvailable())
-            createCheckNetworkDialog();
+                if (!isNetworkAvailable())
+                    createCheckNetworkDialog();
 
-            adapter = new ArticleAdapter();
-            rvResult.setAdapter(adapter);
-            mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-            rvResult.setLayoutManager(mLayoutManager);
-            map = new HashMap();
+                adapter = new ArticleAdapter();
+                rvResult.setAdapter(adapter);
+                mLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+                rvResult.setLayoutManager(mLayoutManager);
+                map = new HashMap();
 
-            load(map);
-            rvResult.addOnScrollListener(new EndlessRecyclerViewScrollListener(mLayoutManager) {
-                @Override
-                public void onLoadMore(int page, int totalItemsCount) {
-                    map.put("page", String.valueOf(page));
-                    loadMore(map);
-                }
-            });
-
+                load(map);
+                rvResult.addOnScrollListener(new EndlessRecyclerViewScrollListener(mLayoutManager) {
+                    @Override
+                    public void onLoadMore(int page, int totalItemsCount) {
+                        map.put("page", String.valueOf(page));
+                        loadMore(map);
+                    }
+                });
             adapter.setListClickListener(new ArticleAdapter.ListClickListener() {
                 @Override
                 public void onArticleItemClick(Article article) {
@@ -106,58 +105,58 @@
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_actionbar, menu);
-        searchItem = menu.findItem(R.id.action_search);
-        sortItem = menu.findItem(R.id.action_sort);
-
-        final SearchView searchView1 = (SearchView) MenuItemCompat.getActionView(sortItem);
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                map.clear();
-                map.put("q", query);
-                map.put("page", "0");
-                load(map);
-                searchView.clearFocus();
+            public boolean onCreateOptionsMenu(Menu menu) {
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.menu_actionbar, menu);
+                searchItem = menu.findItem(R.id.action_search);
+                sortItem = menu.findItem(R.id.action_sort);
+
+                final SearchView searchView1 = (SearchView) MenuItemCompat.getActionView(sortItem);
+                final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        map.clear();
+                        map.put("q", query);
+                        map.put("page", "0");
+                        load(map);
+                        searchView.clearFocus();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        return false;
+                    }
+                });
+
+                return super.onCreateOptionsMenu(menu);
+
+            }
+
+
+
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                int id = item.getItemId();
+
+                if ( id == R.id.action_search)
+                {
+
+                }
+                else if (id == R.id.action_sort)
+                {
+                    final SortDialog sortDialog = new SortDialog();
+                    sortDialog.showDialog(MainActivity.this);
+                    ArrayList<Article> articles  =  new ArrayList<>();
+                    sortDialog.setOnClickButton(map,sortItem,articles,adapter);
+                }
+
                 return true;
             }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-
-    }
-
-
-
-   @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-            if ( id == R.id.action_search)
-            {
-
-            }
-            else if (id == R.id.action_sort)
-            {
-                final SortDialog sortDialog = new SortDialog();
-                sortDialog.showDialog(MainActivity.this);
-                ArrayList<Article> articles  =  new ArrayList<>();
-                sortDialog.setOnClickButton(map,sortItem,articles,adapter);
-            }
-
-        return true;
-    }
-
-    private void createCheckNetworkDialog ()
+            private void createCheckNetworkDialog ()
             {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage("Connect to wifi or quit?")
@@ -176,75 +175,74 @@
                 alert.show();
             }
 
-    private void load(Map<String,String> map)
-    {
-        pbLoading.setVisibility(View.VISIBLE);
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        for(String key:map.keySet())
-        {
-            params.put(key, map.get(key));
-        }
-        params.put("api-key", Constant.API_KEY);
-        client.get(Constant.URL, params, new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+            private void load(Map<String,String> map)
+            {
+                pbLoading.setVisibility(View.VISIBLE);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                for(String key:map.keySet())
+                {
+                    params.put(key, map.get(key));
+                }
+                params.put("api-key", Constant.API_KEY);
+                client.get(Constant.URL, params, new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                        pbLoading.setVisibility(View.GONE);
+                        Gson gson = new GsonBuilder().create();
+                        // Define Response class to correspond to the JSON response returned
+                        JsonObject root = gson.fromJson(responseString, JsonObject.class);
+                        ArrayList<Article> articles = new ArrayList<>();
+
+                        JsonArray jsonArray = root.getAsJsonObject("response").getAsJsonArray("docs");
+                        articles.addAll(Article.fromJsonArray(jsonArray));
+                        adapter.setArticle(articles);
+
+                    }
+                });
             }
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                pbLoading.setVisibility(View.GONE);
-                Gson gson = new GsonBuilder().create();
-                // Define Response class to correspond to the JSON response returned
-                JsonObject root = gson.fromJson(responseString, JsonObject.class);
-                ArrayList<Article> articles = new ArrayList<>();
+            private void loadMore(Map<String, String> map)
+            {
 
-                JsonArray jsonArray = root.getAsJsonObject("response").getAsJsonArray("docs");
-                articles.addAll(Article.fromJsonArray(jsonArray));
-                adapter.setArticle(articles);
+                pbLoadingMore.setVisibility(View.VISIBLE);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
 
+                for(String key:map.keySet())
+                {
+                    params.put(key, map.get(key));
+                }
+                params.put("api-key", Constant.API_KEY);
+                client.get(Constant.URL, params, new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                        pbLoadingMore.setVisibility(View.GONE);
+                        Gson gson = new GsonBuilder().create();
+                        // Define Response class to correspond to the JSON response returned
+                        JsonObject root = gson.fromJson(responseString, JsonObject.class);
+                        ArrayList<Article> articles = new ArrayList<>();
+
+                        JsonArray jsonArray = root.getAsJsonObject("response").getAsJsonArray("docs");
+                        articles.addAll(Article.fromJsonArray(jsonArray));
+                        adapter.addArticle(articles);
+
+                    }
+                });
             }
-        });
-    }
 
-    private void loadMore(Map<String, String> map)
-    {
-
-        pbLoadingMore.setVisibility(View.VISIBLE);
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-
-        for(String key:map.keySet())
-        {
-            params.put(key, map.get(key));
-        }
-        params.put("api-key", Constant.API_KEY);
-        client.get(Constant.URL, params, new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-
+            private Boolean isNetworkAvailable() {
+                ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+                return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
             }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                pbLoadingMore.setVisibility(View.GONE);
-                Gson gson = new GsonBuilder().create();
-                // Define Response class to correspond to the JSON response returned
-                JsonObject root = gson.fromJson(responseString, JsonObject.class);
-                ArrayList<Article> articles = new ArrayList<>();
-
-                JsonArray jsonArray = root.getAsJsonObject("response").getAsJsonArray("docs");
-                articles.addAll(Article.fromJsonArray(jsonArray));
-                adapter.addArticle(articles);
-
-            }
-        });
-    }
-
-    private Boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
-    }
-
 }
